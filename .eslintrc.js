ac07c4df9aff5cd0c43c4adbb4c0a240c5415bde
Anton Karlsson <isanemka@gmail.com>
2025-01-16 20:04:12 +0100

    Initialize Nuxt project with additional packages

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+};
